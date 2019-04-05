02458c6b859b5b57555abd87e445d87175a4d913
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-04-05 14:55:34 +0530

    app.js updated

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const feedRoutes = require('./routes/feeds')
+const loginRoutes = require('./routes/login')
+const signupRoutes = require('./routes/signup')
 const app = express()
 const MONGODB_URL = `mongodb+srv://${ process.env.MONGO_USER}:${ process.env.MONGO_PASSWORD}@mymongo-smhht.mongodb.net/${ process.env.MONGO_DATABASE}?retryWrites=true`
 const errorController = require('./controllers/errors')
@@ -10,15 +12,19 @@ const errorController = require('./controllers/errors')
 app.use(bodyParser.json()) // aplication/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
 })
 
-app.use(feedRoutes)
+app.use('/login', loginRoutes)
+
+app.use('/signup', signupRoutes)
 
 app.get('/500', errorController.get500)
+
+app.use(feedRoutes)
 
 app.use(errorController.get404)
 
