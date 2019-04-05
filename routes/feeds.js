fc09bedd17f635838e8310269fd8f9fd050f8f96
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-04-05 14:57:22 +0530

    routes for login and signup added

--- routes/feeds.js
+++ routes/feeds.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 const feedController = require('../controllers/feeds')
 
+router.post('/ask', feedController.askQuestion)
+
 router.get('/', feedController.getIndex)
 
 module.exports = router
