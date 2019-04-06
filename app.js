314f77ab3e58e4d76c4731ac18de03b74d26ec3a
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-04-06 16:59:37 +0530

    app.js updated

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ app.use((req, res, next) => {
     res.redirect('/500')
 })
 
+app.use((error, req, res, next) => {
+    // console.log(error);
+    const status = error.statusCode || 500
+    const message = error.message
+    const data = error.data
+    res.status(status).json({ message: message, data: data })
+})
+
 mongoose.connect(MONGODB_URL, {useNewUrlParser: true})
     .then(client => {
         console.log('[#] Connected to db')
