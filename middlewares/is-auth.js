a936965303fb836e357c4306731974a71c66dedd
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-04-06 16:57:46 +0530

    authentication middleware updated

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -1,9 +1,20 @@
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
     const auth = req.get('Authorization')
     if(!auth){
         err = new Error('Not Authorized :(')
+        err.statusCode = 401
         throw err
     }
-    auth = auth.split(' ')[1]
+    token = auth.split(' ')[1]
     // verify the jwt token
+    validToken = jwt.verify(token, process.env.WEB_TOKEN_KEY)
+    if(!validToken){
+        err = new Error('Not a Valid User :( ')
+        err.statusCode = 401
+        throw err
+    }
+    req.username = validToken.username
+    next()
 }
